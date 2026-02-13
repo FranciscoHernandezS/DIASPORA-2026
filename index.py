1dd34feea0e55b20dba3c1b143aece54e4c14e0d
Francisco Hernandez Soto <centenaria@gmail.com>
2026-02-12 20:51:35 -0400

    listo

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 numeros = ['infotep', 5 ,7.8 ,False,1]
 # print(f'\nlos elementos de la lista : {numeros}')
 
-print(f'\nla primera posicion es : {numeros[1]}')
+# print(f'\nla primera posicion es : {numeros[1]}')
+
